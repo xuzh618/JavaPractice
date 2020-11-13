3d22588a45a6e3d3b91edb194b4d2f995b2c3b51
tiger <zhaohu.xu@gmail.com>
2020-11-13 11:14:00 +0800

    [mod] add method create listnode

--- src/com/tiger/algorithm/Entity/ListNode.java
+++ src/com/tiger/algorithm/Entity/ListNode.java
@@ -13,4 +13,23 @@ public class ListNode {
         this.val = val;
         next = null;
     }
+
+    public static void createListNode(int[] arry) {
+
+        if (arry == null || arry.length < 1) {
+            return;
+        }
+
+        ListNode head = new ListNode(arry[0]);
+        ListNode nextNode;
+        nextNode = head;
+
+        for (int i = 1; i < arry.length; i++) {
+            ListNode node = new ListNode(arry[i]);
+            nextNode.next = node;
+            nextNode = nextNode.next;
+        }
+
+        nextNode = head;
+    }
 }
