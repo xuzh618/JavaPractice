9e747c254d6b4f4ce2a19334f5d771bf94d3e8a2
tiger02 <zhaohu.xu@gmail.com>
2020-11-16 21:12:08 +0800

    [add] add insert sort

--- src/com/tiger/algorithm/DemoBaseSorts.java
+++ src/com/tiger/algorithm/DemoBaseSorts.java
@@ -9,7 +9,8 @@ public class DemoBaseSorts {
         int[] arr = new int[]{4, 2, 6, 3, 5, 9, 7, 1, 8, 0};
 
 
-        printArr(bubbleSort(arr));
+        //printArr(bubbleSort(arr));
+        printArr(insertSort(arr));
 
     }
 
@@ -54,6 +55,46 @@ public class DemoBaseSorts {
                 break;
             }
 
+
+        }
+
+        return arr;
+    }
+
+
+    /**
+     * 题目：插入排序（打扑克）
+     * 与冒泡排序对比：
+     * <p>
+     * 在冒泡排序中，经过每一轮的排序处理后，数组后端的数是排好序的。
+     * 在插入排序中，经过每一轮的排序处理后，数组前端的数是排好序的。
+     * 插入排序的算法思想是：不断将尚未排好序的数插入到已经排好序的部分。
+     *
+     * @param arr
+     * @return
+     */
+    public static int[] insertSort(int[] arr) {
+
+        if (arr == null) {
+            return null;
+        }
+
+        for (int i = 0; i < arr.length - 1; i++) {
+
+            // 1. get the insert value
+
+            int curValue = arr[i];
+            int j = i;
+            // 2. find the location
+            while (j > 0 && curValue < arr[j - 1]) {
+
+                arr[j] = arr[j - 1];
+                j--;
+            }
+            if (j != i) {
+                //3. insert
+                arr[j] = curValue;
+            }
 
         }
 
