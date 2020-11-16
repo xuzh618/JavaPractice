7220f5171a29cbdc81eeaa747d10dc78c048d49a
tiger02 <zhaohu.xu@gmail.com>
2020-11-16 09:27:01 +0800

    [add] 1.reverse listnode 2. add explain for dellistnode

--- src/com/tiger/algorithm/DemoDeleteListNode.java
+++ src/com/tiger/algorithm/DemoDeleteListNode.java
@@ -2,10 +2,21 @@ package com.tiger.algorithm;
 
 import com.tiger.algorithm.Entity.ListNode;
 
+/**
+ *题目描述
+ * 输入一个链表的头节点，从尾到头反过来返回每个节点的值（用数组返回）。
+ *
+ * 示例 1：
+ *
+ * 输入：head = [1,3,2]
+ * 输出：[2,3,1]
+ * 限制：
+ *
+ * 0 <= 链表长度 <= 10000
+ */
 public class DemoDeleteListNode {
 
-
-    public ListNode delListNode(ListNode head, int val){
+    public ListNode delListNode(ListNode head, int val) {
 
         // create  pre listnode
         ListNode dummy = new ListNode(0);
@@ -13,9 +24,9 @@ public class DemoDeleteListNode {
 
         ListNode pre = dummy, p = head;
 
-        while (p!=null){
+        while (p != null) {
 
-            if(p.val == val){
+            if (p.val == val) {
                 pre.next = p.next;
                 //dummy = pre;
                 break;
