bb0d8825cd0c8987c8ac3aa6aa3d65726ab7673b
tiger <zhaohu.xu@gmail.com>
2020-12-23 10:12:12 +0800

    [mod] add explain for the composition pattern desgin

--- src/com/tiger/design/DemoCompositionTreeNode.java
+++ src/com/tiger/design/DemoCompositionTreeNode.java
@@ -1,12 +1,16 @@
 package com.tiger.design;
 
-import sun.lwawt.macosx.CSystemTray;
-
 import java.util.Enumeration;
 import java.util.Vector;
 
 /**
  * tree structure
+ *
+ * 回到正题，组合模式中当然在组合对象中含有被组合对象的引用，只是不同的是，
+ * 组合模式在概念上更加严格，通常是指引用的被组合对象类型就是组合对象的类型。
+ * 如此一来，使得组合对象和被组合对象处理起来具有一致性。当然，前提是组合被
+ * 对象和被组合对象在本身的概念层次上具有此一致性。
+ * 参考：https://www.cnblogs.com/lwbqqyumidi/p/3758182.html
  */
 public class DemoCompositionTreeNode {
 
